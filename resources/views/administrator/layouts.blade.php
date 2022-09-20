236829967788753d9c7be02ef50c2226eda8606e
Lambertn33 <lambertgithub@gmail.com>
2022-09-20 19:10:47 +0200

    Dashboard Logo

--- resources/views/administrator/layouts.blade.php
+++ resources/views/administrator/layouts.blade.php
@@ -25,6 +25,10 @@
     </head>
 
     <style>
+        .logo-img {
+            margin-top: 1.25rem;
+            width: 90%;
+        }
         table.datatable.no-footer {
             border: none;
         }
@@ -205,7 +209,7 @@
                             </div>
                             <div class="offcanvas offcanvas-start" tabindex="-1" id="adminProfile" aria-labelledby="offcanvasExampleLabel">
                                 <div class="offcanvas-header">
-                                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">My Profile</h5>
                                     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                                 </div>
                                 <div class="offcanvas-body">
@@ -215,27 +219,13 @@
                                                 <div class="card">
                                                     <div class="card-body p-0">
                                                         <div class="user-profile-img">
-                                                                <div style="height: 120px;background-color:transparent">
-                                                                </div>
+                                                            <div style="height: 120px;background-color:transparent">
+                                                            </div>
                                                             <div class="overlay-content rounded-top">
                                                                 <div>
                                                                     <div class="user-nav p-3">
                                                                         <div class="d-flex justify-content-end">
-                                                                            <div class="dropdown">
-                                                                                <a class="dropdown-toggle" href="#" role="button"
-                                                                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                                                                    <i data-eva="more-horizontal-outline" data-eva-width="20" data-eva-height="20"
-                                                                                        class="fill-white"></i>
-                                                                                </a>
-                        
-                                                                                <ul class="dropdown-menu dropdown-menu-end">
-                                                                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                                                                    <li><a class="dropdown-item" href="#">Another action</a>
-                                                                                    </li>
-                                                                                    <li><a class="dropdown-item" href="#">Something else
-                                                                                            here</a></li>
-                                                                                </ul>
-                                                                            </div>
+                                                                            
                                                                         </div>
                                                                     </div>
                                                                 </div>
@@ -246,120 +236,14 @@
                                                         <div class="mt-n5 position-relative">
                                                             <div class="text-center">
                                                                 <img src="/assets/images/profile.png" alt=""
-                                                                    class="" style="width: 30;border-radius:100%">
+                                                                    class="" style="width: 100;border-radius:100%">
                                                                 <div class="mt-3">
                                                                     <h5 class="mb-1">{{Auth::user()->names}}</h5>
                                                                     <p class="text-muted">{{Auth::user()->email}}</p>
                                                                 </div>
                                                             </div>
                                                         </div>
-                        
-                                                        <div class="p-3">
-                                                            <div class="row text-center pb-3">
-                                                                <div class="col-6 border-end">
-                                                                    <div class="p-1">
-                                                                        <h5 class="mb-1">1,269</h5>
-                                                                        <p class="text-muted mb-0">Products</p>
-                                                                    </div>
-                                                                </div>
-                                                                <div class="col-6">
-                                                                    <div class="p-1">
-                                                                        <h5 class="mb-1">5.2k</h5>
-                                                                        <p class="text-muted mb-0">Followers</p>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                        
-                                                            <hr class="mb-4">
-                        
-                                                            
-                                                            <div class="mb-4">
-                                                                <div class="d-flex align-items-start">
-                                                                    <div class="flex-grow-1">
-                                                                        <h5 class="card-title mb-3">Earning</h5>
-                                                                    </div>
-                                                                    <div>
-                                                                        <button class="btn btn-link py-0 shadow-none"  data-bs-toggle="tooltip" data-bs-placement="left" data-bs-trigger="hover" title="Info">
-                                                                            <i data-eva="info-outline" class="fill-muted" data-eva-height="20" data-eva-width="20"></i>
-                                                                        </button>
-                                                                    </div>
-                                                                </div>
-                        
-                                                                <div id="chart-radialBar" class="apex-charts" data-colors='["#3b76e1"]'></div>
-                        
-                                                                <div class="text-center mt-4">
-                                                                    <h4>$26,256</h4>
-                                                                    <p class="text-muted">Earning this Month</p>
-                                                                    <div
-                                                                        class="d-flex align-items-start justify-content-center gap-2">
-                                                                        <div class="badge rounded-pill font-size-13 badge-soft-success">+ 2.65%
-                                                                        </div>
-                                                                        <div class="text-muted text-start text-truncate">From previous period</div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                        
-                                                            <hr class="mb-4">
-                                                            <div class="px-4 mx-n3" data-simplebar style="height: 258px;">
-                        
-                                                                <div>
-                                                                    <h5 class="card-title mb-3">Recent Activity</h5>
-                        
-                                                                    <ul class="list-unstyled mb-0">
-                                                                        <li class="py-2">
-                                                                            <div class="d-flex align-items-start">
-                                                                                <div class="flex-shrink-0 me-3">
-                                                                                    <div class="avatar-md h-auto p-1 py-2 bg-light rounded">
-                                                                                        <div class="text-center">
-                                                                                            <h5 class="mb-0">12</h5>
-                                                                                            <div>Sep</div>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="flex-grow-1 pt-2 text-muted">
-                                                                                    <p class="mb-0">Responded to need “Volunteer Activities"</p>
-                                                                                </div>
-                                                                            </div>
-                                                                        </li>
-                        
-                                                                        <li class="py-2">
-                                                                            <div class="d-flex align-items-start">
-                                                                                <div class="flex-shrink-0 me-3">
-                                                                                    <div class="avatar-md h-auto p-1 py-2 bg-light rounded">
-                                                                                        <div class="text-center">
-                                                                                            <h5 class="mb-0">11</h5>
-                                                                                            <div>Sep</div>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="flex-grow-1 pt-2 text-muted">
-                                                                                    <p class="mb-0">Everyone realizes would be desirable... <a href="javascript: void(0);">Read more</a></p>
-                                                                                </div>
-                                                                            </div>
-                                                                        </li>
-                                                                        <li class="py-2">
-                                                                            <div class="d-flex align-items-start">
-                                                                                <div class="flex-shrink-0 me-3">
-                                                                                    <div class="avatar-md h-auto p-1 py-2 bg-light rounded">
-                                                                                        <div class="text-center">
-                                                                                            <h5 class="mb-0">10</h5>
-                                                                                            <div>Sep</div>
-                                                                                        </div>
-                                                                                    </div>
-                                                                                </div>
-                                                                                <div class="flex-grow-1 pt-2 text-muted">
-                                                                                    <p class="mb-0">
-                                                                                        Joined the group “Boardsmanship Forum”</p>
-                                                                                </div>
-                                                                            </div>
-                                                                        </li>
-                                                                        <li class="pt-2">
-                                                                            <a href="#" class="btn btn-link w-100 shadow-none"><i class="mdi mdi-loading mdi-spin me-2"></i> Load More</a>
-                                                                        </li>
-                                                                    </ul>
-                                                                </div>
-                                                            </div>
-                                                        </div>
+                    
                                                     </div>
                                                     <!-- end card body -->
                                                 </div>
@@ -378,13 +262,7 @@
 
                 <!-- LOGO -->
                 <div class="navbar-brand-box">
-                    <a href="#" class="logo logo-dark">
-                       <h2>LOGO</h2>
-                    </a>
-
-                    <a href="#" class="logo logo-light">
-                       <h2>LOGO</h2>
-                    </a>
+                    <img src="/assets/images/umurinzi_logo.jpg" width="100%" alt="" class="logo-img">
                 </div>
 
                 <button type="button" class="btn btn-sm px-3 header-item vertical-menu-btn topnav-hamburger">
